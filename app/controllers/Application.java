4b9505b48aa0e0133cc8e4ce84a7274ad099ec8c
JulienD <jderenty@gmail.com>
2014-03-21 09:58:26 +0100

    interface to write a message and post it on registered fb accounts

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,11 +5,48 @@ import play.mvc.*;
 
 import views.html.*;
 
+import models.*;
+
+import com.restfb.Connection;
+import com.restfb.DefaultFacebookClient;
+import com.restfb.Parameter;
+import com.restfb.types.FacebookType;
+import play.data.DynamicForm;
+
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render());
     }
+
+    public static Result post() {
+        return ok(post.render());
     }
 
+    public static Result submitPost() {
+        DynamicForm form = play.data.Form.form().bindFromRequest();
+        String text = form.data().get("message");
+        String link = form.data().get("link");
+        String picture = form.data().get("picture");
+        if(text != null) {
+
+            for (IdentityId identityId : IdentityId.findAll()) {
+                try {
+
+                    new DefaultFacebookClient("640059432708885|272b96015a4bae0b280756f9b1b60ebc")
+                            .publish(identityId.userId + "/feed",
+                                    FacebookType.class,
+                                    Parameter.with("message", text),
+                                    Parameter.with("link", link),
+                                    Parameter.with("picture", picture)
+                            );
+                    Logger.info("postSent");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return ok("posté");
+    }
 }
--- build.sbt
+++ build.sbt
@@ -7,7 +7,8 @@ libraryDependencies ++= Seq(
   javaEbean,
   cache,
   "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
-  "ws.securesocial" %% "securesocial" % "master-SNAPSHOT"
+  "ws.securesocial" %% "securesocial" % "master-SNAPSHOT",
+  "com.restfb" % "restfb" % "1.6.14"
 )
 
 resolvers += Resolver.sonatypeRepo("snapshots")
