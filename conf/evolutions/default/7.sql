8ccf208b16182e2e1ff80ac08065ea2c38bb107b
JulienD <jderenty@gmail.com>
2014-04-25 12:02:28 +0200

    evolution DB

---
+++ conf/evolutions/default/7.sql
@@ -0,0 +1,20 @@
+# Picture schema
+
+# --- !Ups
+create table picture (
+  id					varchar(40) not null,
+  type      			text,
+  version               text,
+  constraint pk_picture_id primary key (id)
+);
+
+alter table app add column background_picture_id varchar(40);
+alter table app add column middle_picture_id varchar(40);
+alter table app add column message text;
+
+
+# --- !Downs
+DROP TABLE picture CASCADE;
+ALTER TABLE app DROP background_picture_id;
+ALTER TABLE app DROP middle_picture_id;
+ALTER TABLE app DROP message;
