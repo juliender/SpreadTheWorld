b46bdb155527db9b263be286469b296569861b3a
JulienD <jderenty@gmail.com>
2014-04-28 11:22:45 +0200

    js check input and links  and flash error messages

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -13,9 +13,27 @@ $(document).ready(function() {
         document.getElementById("midpic").click();
     });
 
+    initCheckInput($('.js_fbPostMessage'));
+    initCheckInput($('.js_modifMessage'));
 
+    initCheckLinks();
 });
 
+
+var urlPattern = new RegExp('(http|ftp|https)://[a-z0-9\-_]+(\.[a-z0-9\-_]+)+([a-z0-9\-\.,@\?^=%&;:/~\+#]*[a-z0-9\-@\?^=%&;/~\+#])?', 'i');
+
+function initCheckLinks(){
+    var inputLink=$(".js_link");
+    inputLink.on("keyup", function(){
+
+        if(!urlPattern.test(inputLink.val())){
+            $(".js_link_error").removeClass("hide");
+        }else{
+            $(".js_link_error").addClass("hide");
+        }
+    });
+
+}
 
 function initCheckBtn() {
 	$(".js_check").on("click", function(ev){
@@ -23,4 +41,18 @@ function initCheckBtn() {
 	    checkbox.prop('checked', !checkbox.is(":checked"));
 	    $(this).find(".js_checkIcon").toggle();
 	});
+}
+
+function initCheckInput(textarea){
+
+    textarea.on("keyup", function(){
+    var count=$(this).val().length;
+    var countDiv=$(this).parent().find(".js_count");
+        countDiv.html(count);
+        if(count>300){
+            countDiv.css("color","red");
+        }else{
+            countDiv.css("color","");
+        }
+    });
 }
